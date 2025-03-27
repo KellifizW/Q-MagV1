6d78b152334144a0051d9103fcd7ca12b4bfa264
KellifizW <kellyindabox@gmail.com>
2025-03-27 13:19:00 +0800

    Update database.py

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ import time
 
 REPO_DIR = "repo"
 DB_PATH = os.path.join(REPO_DIR, "stocks.db")
-REPO_URL = f"https://{st.secrets['TOKEN']}@github.com/YOUR_USERNAME/stock-screening.git"  # 替換 YOUR_USERNAME
+REPO_URL = f"https://{st.secrets['TOKEN']}@github.com/KellifizW/Q-MagV1.git"  # 替換 YOUR_USERNAME
 nasdaq = mcal.get_calendar('NASDAQ')
 
 def clone_repo():
